e51a535336e424f593e8623e615fcfd09d63c3ea
Reece Pierson <piersonreece@gmail.com>
2022-01-11 22:47:10 -0700

    working webpack config update

---
+++ build.js
@@ -0,0 +1,6 @@
+const rewire = require('rewire')
+const defaults = rewire('react-scripts/scripts/build.js')
+const config = defaults.__get__('config')
+
+config.output.filename = 'static/js/plants_manager.js'
+config.plugins[5].options.filename = 'static/css/plant_manager.css'
